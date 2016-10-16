bd793cb1297682db90bc737eb17c6ae73ede3d36
and85 <andreytkach85@gmai.com>
2016-10-16 18:09:50 +0200

    Array left rotation

---
+++ DataStructures/Arrays/array-left-rotation/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    // https://www.hackerrank.com/challenges/array-left-rotation
+    static void Main(String[] args)
+    {
+        string[] firstLine = Console.ReadLine()
+            .Split(' ');
+        int n = int.Parse(firstLine[0]);
+        int d = int.Parse(firstLine[1]);
+
+        int[] arr = new int[n];
+        arr = Console.ReadLine()
+            .Split(' ')
+            .Select(x => int.Parse(x))
+            .ToArray();
+
+        Rotate(n, d, arr);
+
+        string result = string.Join(" ", arr);
+        Console.WriteLine(result);
+    }
+
+    static void Rotate(int n, int d, int[] arr)
+    {
+        Array.Reverse(arr, 0, d);
+        Array.Reverse(arr, d, n - d);
+        Array.Reverse(arr, 0, n);
+    }
+}
