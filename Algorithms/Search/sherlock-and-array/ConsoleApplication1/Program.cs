6a25bda8a5db22d256be679f33c10698233159f4
and85 <andreytkach85@gmai.com>
2016-10-17 21:13:00 +0200

    added sherlock-and-array and runningtime

---
+++ Algorithms/Search/sherlock-and-array/ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    //https://www.hackerrank.com/challenges/sherlock-and-array
+    static void Main(String[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = (from s in Console.ReadLine().Split() select Convert.ToInt32(s)).ToArray();
+
+            if (IsElementExist(n, array))
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+
+    static bool IsElementExist(int n, int[] array)
+    {
+        var leftSums = new int[n];
+        var rightSums = new int[n];
+
+        int leftSum = 0;
+        int rightSum = 0;
+        for (int i = 0, j = n - 1; i < n; i++, j--)
+        {
+            leftSum += array[i];
+            leftSums[i] = leftSum;
+
+            rightSum += array[j];
+            rightSums[j] = rightSum;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (leftSums[i] == rightSums[i])
+                return true;
+        }
+
+        return false;
+    }
+}
---
+++ Algorithms/Sorting/runningtime/ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        Console.ReadLine();
+        int[] _ar = (from s in Console.ReadLine().Split() select Convert.ToInt32(s)).ToArray();
+
+
+        int result = insertionSort(_ar);
+        Console.WriteLine(result);
+    }
+
+    public static int insertionSort(int[] A)
+    {
+        int result = 0;
+
+        var j = 0;
+        for (var i = 1; i < A.Length; i++)
+        {
+            var value = A[i];
+            j = i - 1;
+            while (j >= 0 && value < A[j])
+            {
+                A[j + 1] = A[j];
+                j = j - 1;
+                result++;
+
+            }
+            A[j + 1] = value;
+
+        }
+
+        return result;
+    }
+}
