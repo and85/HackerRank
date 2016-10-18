c30e8b4e6a56b7f0b333128306782c8262a7c54b
and85 <andreytkach85@gmai.com>
2016-10-18 22:53:12 +0200

    pangrams

---
+++ Algorithms/Strings/pangrams/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        string input = Console.ReadLine();
+        if (IsPangram(input))
+            Console.WriteLine("pangram");
+        else
+            Console.WriteLine("not pangram");
+
+#if DEBUG
+        Console.WriteLine("END");
+        Console.ReadLine();
+#endif
+    }
+
+    static bool IsPangram(string input)
+    {
+        const byte NumberOfLetters = 26;
+        var uniqueLetters = new BitArray(NumberOfLetters);
+        byte index = 0;
+        byte aIndex = 97;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i] == ' ')
+                continue;
+
+            byte symbol = Convert.ToByte(Char.ToLower(input[i]));
+            index = Convert.ToByte(symbol - aIndex);
+            uniqueLetters.Set(index, true);            
+        }
+
+        int numberOfSetBits =  
+            (from bool m in uniqueLetters where m select m).Count();
+
+        if (numberOfSetBits == NumberOfLetters)
+            return true;
+        else
+            return false;
+    }
+}
