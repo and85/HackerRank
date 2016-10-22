f620228d874d333cb5dda299e0c8e61cd6b9f633
and85 <andreytkach85@gmai.com>
2016-10-22 16:37:00 +0200

    sansa and xor

---
+++ Algorithms/BitManipulation/sansa-and-xor/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    // https://www.hackerrank.com/challenges/sansa-and-xor
+    static void Main(String[] args)
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            arr = Console.ReadLine()
+                .Split(' ')
+                .Select(x => int.Parse(x))
+                .ToArray();
+
+            int result = GetXorOfSubsets(arr, n);
+            Console.WriteLine(result);
+        }
+    }
+
+    static int GetXorOfSubsets(int[] arr, int n)
+    {
+        int result = 0;
+
+        if (n % 2 == 0)
+            return 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0)
+                result ^= arr[i];
+        }
+
+        return result;
+    }
+}
