6316d9063a0ac4292f182200bf88481effdf3c69
and85 <andreytkach85@gmai.com>
2016-10-23 11:48:31 +0200

    simple-text-editor

---
+++ DataStructures/Stacks/simple-text-editor/ConsoleApplication1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    // https://www.hackerrank.com/challenges/simple-text-editor
+    static void Main(String[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine());
+        string[] command;
+
+        for (int i = 0; i < q; i++)
+        {
+            command = Console.ReadLine().Split(' ');
+            switch (command[0])
+            {
+                case "1":
+                    Append(command[1]);
+                    break;
+                case "2":
+                    Delete(Convert.ToInt32(command[1]));
+                    break;
+                case "3":
+                    Print(Convert.ToInt32(command[1]));
+                    break;
+                case "4":
+                    Undo();
+                    break;
+            }
+        }
+    }
+
+    static string _internalString = string.Empty;
+    static Stack<string> _internalStates = new Stack<string>();
+
+    static void Append(string w)
+    {
+        _internalStates.Push(_internalString);
+        _internalString += w;
+    }
+
+    static void Delete(int k)
+    {
+        _internalStates.Push(_internalString);
+        _internalString = _internalString.Remove(_internalString.Length - k);
+    }
+
+    static void Print(int k)
+    {
+        Console.WriteLine(_internalString[k - 1]);
+    }
+
+    static void Undo()
+    {
+        _internalString = _internalStates.Pop();
+    }
+}
