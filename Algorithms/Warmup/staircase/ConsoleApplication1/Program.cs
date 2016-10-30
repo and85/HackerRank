538e745b4a6a3a144a19b13c4ac605009a1ac5de
and85 <andreytkach85@gmai.com>
2016-10-30 23:08:57 +0100

    staircase

---
+++ Algorithms/Warmup/staircase/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    // https://www.hackerrank.com/challenges/staircase
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string line = string.Empty.PadLeft(n - i - 1, ' ') + string.Empty.PadLeft(i + 1, '#');
+            Console.WriteLine(line);
+        }
+    }
+}
