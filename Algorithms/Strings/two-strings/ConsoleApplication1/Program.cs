2546f0f65bab096cba64e8926cdf90c8fb16d9af
and85 <andreytkach85@gmai.com>
2016-10-30 22:43:09 +0100

    two strings

---
+++ Algorithms/Strings/two-strings/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    // https://www.hackerrank.com/challenges/two-strings
+    static void Main(String[] args)
+    {
+        int p = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < p; i++)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+
+            if (IsSubstring(a, b))
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+
+    static bool IsSubstring(string a, string b)
+    {
+        var bitArray = new BitArray(26);
+
+        foreach (char c in a)
+        {
+            bitArray.Set(c - 97, true);
+        }
+
+        foreach (char c in b)
+        {
+            if (bitArray.Get(c - 97))
+                return true;
+        }
+
+        return false;
+    }
+}
