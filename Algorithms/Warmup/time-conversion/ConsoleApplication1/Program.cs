b69384fa582457757157e9f81b1e8fc11886cf83
and85 <andreytkach85@gmai.com>
2016-11-01 00:49:01 +0100

    time-convertion

---
+++ Algorithms/Warmup/time-conversion/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    // https://www.hackerrank.com/challenges/time-conversion
+    static void Main(String[] args)
+    {
+        string time = Console.ReadLine();
+        DateTime dateTime = DateTime.Parse(time, CultureInfo.InvariantCulture);
+        string result = dateTime.ToString("HH:mm:ss", CultureInfo.InvariantCulture);
+
+        Console.WriteLine(result);
+    }
+}
