027e2e4c3108813c010acc4a233ba798fe75fbd7
and85 <andreytkach85@gmai.com>
2016-11-15 22:39:25 +0100

    beautiful-days-at-the-movies

---
+++ Algorithms/Implementation/beautiful-days-at-the-movies/ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    // https://www.hackerrank.com/challenges/beautiful-days-at-the-movies
+    static void Main(String[] args)
+    {
+        string input = Console.ReadLine();
+        int i = Convert.ToInt32(input.Split(' ')[0]);
+        int j = Convert.ToInt32(input.Split(' ')[1]);
+        int k = Convert.ToInt32(input.Split(' ')[2]);
+
+        int counter = 0;
+
+        for (int a = i; a <= j; a++)
+        {
+            double d = (double)(a - Reverse(a)) / k;
+            if (IsEval(d))
+                counter++;
+        }
+
+        Console.WriteLine(counter);
+
+#if DEBUG
+        Console.WriteLine("END");
+        Console.ReadLine();
+#endif
+    }
+
+    static int Reverse(int x)
+    {
+        string original = x.ToString();
+        // since x is quite short, it's more efficient to use string but not StringBuilder here
+        string reversed = string.Empty;
+
+        for (int i = original.Length - 1; i >= 0; i--)
+        {
+            reversed += original[i];
+        }
+
+        return int.Parse(reversed);
+    }
+
+    static bool IsEval(double d)
+    {
+        return (d % 1) == 0;
+    }
+}
