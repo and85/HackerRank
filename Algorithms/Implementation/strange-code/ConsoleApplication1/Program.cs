b679536ab1ba7d8e67b0df34df3fbe150ae85417
and85 <andreytkach85@gmai.com>
2016-11-20 17:05:58 +0100

    strange code

---
+++ Algorithms/Implementation/strange-code/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    // https://www.hackerrank.com/challenges/strange-code
+    static void Main(String[] args)
+    {
+        long t = long.Parse(Console.ReadLine());
+
+        long beginingIndex = 0;
+        long iteration = 0;
+        FindFirstElementInCycle(t, ref beginingIndex, ref iteration);
+
+        long beginingValue = 3;
+        for (long i = 1; i < iteration; i++)
+        {
+            beginingValue = beginingValue * 2;
+        }
+
+        long result = beginingValue - (t - beginingIndex);
+        Console.WriteLine(result);
+
+#if DEBUG
+        Console.WriteLine("END");
+        Console.ReadLine();
+#endif
+    }
+
+    private static void FindFirstElementInCycle(long t, ref long beginingIndex, ref long iteration)
+    {
+        long step = 0;
+
+        for (long i = 1; i <= t; i = i + step)
+        {
+            beginingIndex = i;
+
+            if (i == 1)
+                step = 3;
+            else
+                step *= 2;
+
+            iteration++;
+        }
+    }
+}
