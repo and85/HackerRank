844d0b68d31cb7b1fc28f1079a56f02b11c87474
and85 <andreytkach85@gmai.com>
2016-11-27 20:23:00 +0100

    extra long factorials

---
+++ Algorithms/Implementation/extra-long-factorials/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ConsoleApplication1
+{
+    // https://www.hackerrank.com/challenges/extra-long-factorials
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string factorial = GetFactorial(n);
+            Console.WriteLine(factorial);
+        }
+
+        static string GetFactorial(int n)
+        {
+            BigInteger factorial = n;
+            for (int i = n-1; i >= 1; i--)
+            {
+                factorial *= i;
+            }
+
+            return factorial.ToString();
+        }
+    }
+}
