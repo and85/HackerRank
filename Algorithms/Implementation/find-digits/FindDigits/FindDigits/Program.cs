43fc2378b9d901ef35b9d4f3e8784c753a7b82ba
and85 <andreytkach85@gmai.com>
2016-11-30 22:59:47 +0100

    find digits

---
+++ Algorithms/Implementation/find-digits/FindDigits/FindDigits/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDigits
+{
+    // https://www.hackerrank.com/challenges/find-digits
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int counter = 0;
+                string str = n.ToString();
+                foreach (var digit in str)
+                {
+                    var curDigit = int.Parse(digit.ToString());
+                    if (curDigit != 0 && (n % curDigit == 0))
+                        counter++;
+                }
+
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
