612b286e3e12d7eb78d38a933c2ab5476bdefba3
and85 <andreytkach85@gmai.com>
2016-12-05 21:26:32 +0100

    designer pdf viewer

---
+++ Algorithms/Implementation/designer-pdf-viewer/DesignerPdfViewer/DesignerPdfViewer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    // https://www.hackerrank.com/challenges/designer-pdf-viewer
+    static void Main(String[] args)
+    {
+        string[] h_temp = Console.ReadLine().Split(' ');
+        int[] h = Array.ConvertAll(h_temp, Int32.Parse);
+        string word = Console.ReadLine();
+
+        int result = GetRectangle(h, word);
+        Console.WriteLine(result);
+    }
+
+    private static int GetRectangle(int[] h, string word)
+    {
+        int maxHeight = int.MinValue;
+        foreach (char symbol in word)
+        {
+            int position = symbol - 'a';
+            if (h[position] > maxHeight)
+                maxHeight = h[position];
+        }
+
+        return maxHeight * word.Length;
+    }
+}
