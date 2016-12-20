80c829dcc26c37a6bf0a499204a53341efd6ec51
and85 <andreytkach85@gmai.com>
2016-12-20 21:51:10 +0100

    bon-appetit

---
+++ Algorithms/Implementation/bon-appetit/BonAppetit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+    // https://www.hackerrank.com/challenges/bon-appetit
+    static void Main(String[] args)
+    {
+        string[] firstLine = Console.ReadLine().Split(' ');
+        int n = int.Parse(firstLine[0]);
+        int k = int.Parse(firstLine[1]);
+        int[] prices = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int charged = int.Parse(Console.ReadLine());
+
+        int actual = 0;
+        for (int i = 0; i < prices.Length; i++)
+        {
+            if (i == k) continue;
+            actual += prices[i];
+        }
+
+        actual /= 2;
+
+        if (actual != charged)
+            Console.WriteLine(charged - actual);
+        else
+            Console.WriteLine("Bon Appetit");
+    }
+}
