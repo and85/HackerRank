c91a790289c965c51b52e2599115cbe0e1e41d75
and85 <andreytkach85@gmai.com>
2016-12-21 22:06:47 +0100

    super reduced string

---
+++ Algorithms/Strings/Super Reduced String/SuperReducedString/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperReducedString
+{
+    // https://www.hackerrank.com/challenges/reduced-string
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string r = ReducedString(s);
+            Console.WriteLine(r);
+#if DEBUG
+            Console.WriteLine("END");
+            Console.ReadLine();
+#endif
+        }
+
+        // brute force O(n^2) solution, hackerrank provides O(n) solution based on stack
+        public static string ReducedString(string s)
+        {
+            string r = s;
+
+            if (HasDuplicates(s))
+            {
+                
+                for (int i = 1; i < s.Length; i++)
+                {
+                    if (s[i] == s[i - 1])
+                    {
+                        r = RemoveChars(s, i - 1, i);
+                        break;
+                    }
+                }
+
+                r = ReducedString(r);
+            }
+
+            return r.Length > 0 ? r : "Empty String";
+        }
+
+        public static bool HasDuplicates(string s)
+        {
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == s[i - 1])
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static string RemoveChars(string input, params int[] indexes)
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!indexes.Contains(i))
+                    sb.Append(input[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
